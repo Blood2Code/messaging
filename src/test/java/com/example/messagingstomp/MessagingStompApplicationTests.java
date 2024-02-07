53b215abd17890eb336e3ba70f185dc271b50293
umar <abduxalilraxmonberdiyev@gmail.com>
2024-02-07 18:56:17 +0500

    initial

---
+++ src/test/java/com/example/messagingstomp/MessagingStompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagingstomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingStompApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
