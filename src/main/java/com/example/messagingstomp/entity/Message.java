493713d681c6ef9c9a72adef5d095d800214cd44
umar <abduxalilraxmonberdiyev@gmail.com>
2024-02-12 18:33:59 +0500

    fix: push for home, don't judge

--- src/main/java/com/example/messagingstomp/entity/Message.java
+++ src/main/java/com/example/messagingstomp/entity/Message.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Message {
-    private String from;
-    private String message;
+    private String name;
 
 }
