e5fb473421b922c2359ed6944b368b92c50eadda
umar <abduxalilraxmonberdiyev@gmail.com>
2024-03-01 18:06:32 +0500

    del: unused code - alternative see EventProvider

--- src/main/java/com/example/messagingstomp/controller/GreetingController.java
+++ src/main/java/com/example/messagingstomp/controller/GreetingController.java
@@ -16,15 +16,4 @@ public class GreetingController {
     public Greeting greeting(Message message) {
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
-
-
-//    @EventListener(SessionConnectedEvent.class)
-//    public void handleConnected(WebSocketSession session) {
-//        String username = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        System.err.println(username);
-//        String channelName = "private-" + username;
-//
-//        userChannels.put(session.getId(), channelName);
-//        messagingTemplate.convertAndSendToUser(username, "/topic/" + channelName, null);
-//    }
 }
